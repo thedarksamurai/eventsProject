7054385f5f26bcb7362c9acf675b6810b5c1b8f2
Skye /thedarksamurai <113961429+thedarksamurai@users.noreply.github.com>
2023-03-07 12:12:13 -0700

    removed a bugged file

--- world-events/pages/events/indexEventsPage.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import EventsPage from './index'
-
-describe('<EventsPage />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<EventsPage />)
-  })
-})
